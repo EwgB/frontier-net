8a7d1d30fb60a5a0990e5204e81afa3b83bd0d72
EwgB <ewgbel@gmail.com>
2012-06-21 00:45:49 +0200

    Converted StdAfx.h to StdAfx.cs

---
+++ Terrain/Utils/StdAfx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Frontier {
+	enum SurfaceColor {
+		SURFACE_COLOR_BLACK,
+		SURFACE_COLOR_SAND,
+		SURFACE_COLOR_DIRT,
+		SURFACE_COLOR_GRASS,
+		SURFACE_COLOR_ROCK,
+		SURFACE_COLOR_SNOW,
+	};
+
+	enum SurfaceType {
+		SURFACE_NULL,
+		SURFACE_SAND_DARK,
+		SURFACE_SAND,
+		SURFACE_DIRT_DARK,
+		SURFACE_DIRT,
+		SURFACE_FOREST,
+		SURFACE_EDGE,
+		SURFACE_GRASS,
+		SURFACE_GRASS_EDGE,
+		SURFACE_DEEPGRASS,
+		SURFACE_ROCK,
+		SURFACE_SNOW,
+		SURFACE_TYPES
+	};
+
+	enum LOD {
+		LOD_LOW,
+		LOD_MED,
+		LOD_HIGH,
+		LOD_LEVELS
+	};
+
+	enum DIRS {
+		NORTH,
+		SOUTH,
+		EAST,
+		WEST
+	};
+
+	struct Cell {
+		float elevation;
+		float water_level;
+		float detail;
+	};
+}
