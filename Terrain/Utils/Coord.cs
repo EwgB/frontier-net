7c242ee14079decfaaae039111a5022b8b74c076
EwgB <ewgbel@gmail.com>
2012-06-20 22:09:30 +0200

    Coord.cpp converted

---
+++ Terrain/Utils/Coord.cs
@@ -0,0 +1,49 @@
+﻿/*-----------------------------------------------------------------------------
+  Coord.cs
+  2011 Shamus Young
+-------------------------------------------------------------------------------
+  Coord is a struct for manipulating a pair of ints. Good for grid-walking.
+-----------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Frontier {
+	struct Coord {
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public Coord(int x, int y)	{ X = x;	Y = y; }
+		public Coord(Coord c)				{ X = c.X; Y = c.Y; }
+
+		public static bool operator ==(Coord a, Coord b)	{ return  (a.X == b.X && a.Y == b.Y); }
+		public static bool operator !=(Coord a, Coord b)	{ return !(a.X == b.X && a.Y == b.Y); }
+
+		public static Coord operator +(Coord a, Coord b) { return new Coord(a.X + b.X, a.Y + b.Y); }
+		public static Coord operator +(Coord c, int i) { return new Coord(c.X + i, c.Y + i); }
+
+		public static Coord operator -(Coord a, Coord b) { return new Coord(a.X - b.X, a.Y - b.Y); }
+		public static Coord operator -(Coord c, int i)		{ return new Coord(c.X - i, c.Y - i);}
+
+		public static Coord operator *(Coord a, Coord b)	{ return new Coord(a.X * b.X, a.Y * b.Y); }
+		public static Coord operator *(Coord c, int i)		{ return new Coord(c.X * i, c.Y * i); }
+
+		bool Walk(int size) { return Walk (size, size); }
+		bool Walk(int x, int y) {
+			X++;
+			if (X >= x) {
+				Y++;
+				X = 0;
+				if (Y >= y) {
+					Y = 0; 
+					return true;
+				}
+			}
+			return false;
+		}
+
+		void Clear() { X = Y = 0; }
+	}
+}
