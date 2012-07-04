41060d9f2fdbf7ccabe24d6ccf962f4c55ae8e71
EwgB <ewgbel@gmail.com>
2012-07-05 01:13:24 +0200

    Some rebuilding and more conversion in Brush.cs

--- Terrain/Brush.cs
+++ Terrain/Brush.cs
@@ -6,137 +6,103 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 using OpenTK;
 using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 
 namespace Frontier {
-	struct tuft { public Vector3[] v = new Vector3[4]; }
-
 	enum BrushStage { Begin, Build, Compile, Done }
 
 	class Brush : GridData {
-		private const int
-			BRUSH_TYPES = 4,
-			MAX_TUFTS   = 9;
-		private const int BRUSH_SIZE = 32;
+		#region Constants, member variables and properties
+		private const int						BRUSH_TYPES = 4;
+		private const int						MAX_TUFTS = 9;
+		private const int						BRUSH_SIZE = 32;
 
-		private static UVBox[]
-			box = new UVBox[BRUSH_TYPES],
-			boxFlower = new UVBox[BRUSH_TYPES];
-		private static bool prep_done;
-		private static tuft[] tuftList = new tuft[MAX_TUFTS];
+		private static UVBox[]			mBox = new UVBox[BRUSH_TYPES];
+		private static UVBox[]			mBoxFlower = new UVBox[BRUSH_TYPES];
+		private static bool					mPrepDone;
+		private static Vector3[,]		mTuftList = new Vector3[MAX_TUFTS, 4];
+		private static VBO					mVBO;
 
-		private BrushStage stage;
-		private int currentDistance;
-		private Coord origin, gridPosition, walk;
-		
+		private BrushStage					mStage;
+		private int									mCurrentDistance;
+		private Coord								mOrigin;
+		private Coord								mGridPosition;
+		private Coord								mWalk;
+		private Mesh								mMesh;
+		private BBox								mBBox;
+
 		// List<GLrgba>  colors;
 		// List<Vector3> vertices;
 		// List<Vector3> normals;
 		// List<Vector2> uvs;
 		// List<UINT>      index;
-		private Mesh        mesh;
-		private static VBO  vbo;
-		private BBox      bbox;
 
 		public bool Valid { get; set; }
-		public bool Ready { get { return stage == BrushStage.Done; } }
+		public bool Ready { get { return mStage == BrushStage.Done; } }
+		#endregion
 
-		private void VertexPush (Vector3 vert, Vector3 normal, Color4 color, Vector2 uv);
+		#region Private methods
+		private void VertexPush(Vector3 vert, Vector3 normal, Color4 color, Vector2 uv);
 		private void QuadPush (int n1, int n2, int n3, int n4);
 
-		public Brush() : base() {
-			origin = new Coord(0, 0);
-			gridPosition = new Coord();
-			walk = new Coord();
-			currentDistance = 0;
-			Valid = false;
-			bbox.Clear();
-			gridPosition.Clear();
-			walk.Clear();
-			mesh.Clear();
-			stage = BrushStage.Begin;
-  
-			if (!prep_done)
-				do_prep ();
-		}
-
-		public void Set(int x, int y, int density) {
-			if (origin.X == x * BRUSH_SIZE && origin.Y == y * BRUSH_SIZE)
-				return;
-
-			gridPosition.X = x;
-			gridPosition.Y = y;
-
-			currentDistance = (uint) Math.Abs(density);
-			origin.X = x * BRUSH_SIZE;
-			origin.Y = y * BRUSH_SIZE;
-			stage = BrushStage.Begin;
-			mesh.Clear ();
-			bbox.Clear ();
-		}
-
-		private static void do_prep() {
-			int      i, j;
-			Matrix4  m;
-			float    angle_step;
-
-			for (i = 0; i < BRUSH_TYPES; i++) {
-				box[i].Set (i, 0, BRUSH_TYPES, 2);
-				box[i].lr.y *= 0.99f;
+		private static void DoPrep() {
+			for (int i = 0; i < BRUSH_TYPES; i++) {
+				mBox[i].Set (i, 0, BRUSH_TYPES, 2);
+				mBox[i].lr.y *= 0.99f;
 			}
-			angle_step = 360.0f / MAX_TUFTS;
-			for (i = 0; i < MAX_TUFTS; i++) {
-				tuftList[i].v[0] = new Vector3(-1, -1, 0);
-				tuftList[i].v[1] = new Vector3(1, -1, 0);
-				tuftList[i].v[2] = new Vector3(1, 1, 0);
-				tuftList[i].v[3] = new Vector3(-1, 1, 0);
-				m.Identity ();
-				m.Rotate (angle_step * (float)i, 0.0f, 0.0f, 1.0f);
-				for (j = 0; j < 4; j++) 
-					tuftList[i].v[j] = m.TransformPoint (tuftList[i].v[j]);
+			float angleStep = 360.0f / MAX_TUFTS;
+			for (int i = 0; i < MAX_TUFTS; i++) {
+				mTuftList[i, 0] = new Vector3(-1, -1, 0);
+				mTuftList[i, 1] = new Vector3( 1, -1, 0);
+				mTuftList[i, 2] = new Vector3( 1,  1, 0);
+				mTuftList[i, 3] = new Vector3(-1,  1, 0);
+				Matrix4 m = Matrix4.CreateRotationZ(angleStep * i);
+				for (int j = 0; j < 4; j++)
+					mTuftList[i, j] = m.TransformPoint(mTuftList[i, j]);
 			}
-			prep_done = true;
+			mPrepDone = true;
 		}
 
 		private bool ZoneCheck() {
-			if (!CachePointAvailable (origin.X, origin.Y))
+			if (!CachePointAvailable (mOrigin.X, mOrigin.Y))
 				return false;
-			if (!CachePointAvailable (origin.X + BRUSH_SIZE, origin.Y))
+			if (!CachePointAvailable (mOrigin.X + BRUSH_SIZE, mOrigin.Y))
 				return false;
-			if (!CachePointAvailable (origin.X + BRUSH_SIZE,origin.Y + BRUSH_SIZE))
+			if (!CachePointAvailable (mOrigin.X + BRUSH_SIZE,mOrigin.Y + BRUSH_SIZE))
 				return false;
-			if (!CachePointAvailable (origin.X, origin.Y + BRUSH_SIZE))
+			if (!CachePointAvailable (mOrigin.X, mOrigin.Y + BRUSH_SIZE))
 				return false;
 			return true;
 		}
 
 		private void Build(long stop) {
-			int world_x = origin.X + walk.X;
-			int world_y = origin.Y + walk.Y;
+			int world_x = mOrigin.X + mWalk.X;
+			int world_y = mOrigin.Y + mWalk.Y;
 
-			if (CacheSurface (world_x, world_y) == SURFACE_GRASS_EDGE) {
-				Region r					= WorldRegionFromPosition (world_x, world_y);
-				int index					= world_x + world_y * BRUSH_SIZE;
-				tuft this_tuft		= tuftList[index % MAX_TUFTS];
-				Vector3 root			= new Vector3(world_x, world_y, 0.0f);
-				float height			= 0.25f + (r.moisture * r.temperature) * 2.0f;
+			if (CacheSurface(world_x, world_y) == SURFACE_GRASS_EDGE) {
+				Region r = FWorld.RegionFromPosition(world_x, world_y);
+				int index = world_x + world_y * BRUSH_SIZE;
+				int thisTuftIndex	= index % MAX_TUFTS;
+				Vector3 root = new Vector3(world_x, world_y, 0);
+				float height = 0.25f + (r.moisture * r.temperature) * 2.0f;
 
-				Vector2 size			= new Vector2(1.0f + WorldNoisef (index) * 1.0f, 1.0f + WorldNoisef (index) * height);
-				size.Y						= Math.Max (size.X, size.Y);		// Don't let bushes get wider than they are tall
+				Vector2 size = new Vector2(
+					1.0f + FWorld.NoiseFloat(index) * 1.0f,
+					1.0f + FWorld.NoiseFloat(index) * height);
+				size.Y = Math.Max (size.X, size.Y);		// Don't let bushes get wider than they are tall
 				
-				Color4 color;
-				color = CacheSurfaceColor (world_x, world_y);
+				Color4 color = CacheSurfaceColor (world_x, world_y);
 				color *= 0.75f;
-				color.alpha = 1.0f;
+				color.A = 1;
 
 				//Now we construct our grass panels
 				Vector3[]	v = new Vector3[8];
-				for (int i = 0; i < 4; i++) { 
-					v[i]				= Vector3.Multiply(this_tuft.v[i], new Vector3(size.X, size.X, 0.0f));
-					v[i + 4]		= Vector3.Multiply(this_tuft.v[i], new Vector3(size.X, size.X, 0.0f));
+				for (int i = 0; i < 4; i++) {
+					v[i] = Vector3.Multiply(mTuftList[thisTuftIndex, i], new Vector3(size.X, size.X, 0));
+					v[i + 4] = Vector3.Multiply(mTuftList[thisTuftIndex, i], new Vector3(size.X, size.X, 0));
 					v[i + 4].Z += size.Y;
 				}
 
@@ -146,55 +112,89 @@ namespace Frontier {
 				}
 
 				int patch = r.flower_shape[index % FLOWERS] % BRUSH_TYPES;
-				int current = mesh.Vertices ();
+				int current = mMesh.VertexCount;
 				Vector3 normal = CacheNormal(world_x, world_y);
 
-				mesh.PushVertex (v[0], normal, color, box[patch].Corner (1)); 
-				mesh.PushVertex (v[1], normal, color, box[patch].Corner (1)); 
-				mesh.PushVertex (v[2], normal, color, box[patch].Corner (0)); 
-				mesh.PushVertex (v[3], normal, color, box[patch].Corner (0)); 
-				mesh.PushVertex (v[4], normal, color, box[patch].Corner (2)); 
-				mesh.PushVertex (v[5], normal, color, box[patch].Corner (2)); 
-				mesh.PushVertex (v[6], normal, color, box[patch].Corner (3)); 
-				mesh.PushVertex (v[7], normal, color, box[patch].Corner (3)); 
-				mesh.PushQuad (current, current + 2, current + 6, current + 4);
-				mesh.PushQuad (current + 1, current + 3, current + 7, current + 5);
+				mMesh.PushVertex (v[0], normal, color, mBox[patch].Corner (1)); 
+				mMesh.PushVertex (v[1], normal, color, mBox[patch].Corner (1)); 
+				mMesh.PushVertex (v[2], normal, color, mBox[patch].Corner (0)); 
+				mMesh.PushVertex (v[3], normal, color, mBox[patch].Corner (0)); 
+				mMesh.PushVertex (v[4], normal, color, mBox[patch].Corner (2)); 
+				mMesh.PushVertex (v[5], normal, color, mBox[patch].Corner (2)); 
+				mMesh.PushVertex (v[6], normal, color, mBox[patch].Corner (3)); 
+				mMesh.PushVertex (v[7], normal, color, mBox[patch].Corner (3)); 
+				mMesh.PushQuad (current, current + 2, current + 6, current + 4);
+				mMesh.PushQuad (current + 1, current + 3, current + 7, current + 5);
 			}
-			if (walk.Walk (BRUSH_SIZE)) 
-				stage++;
+			if (mWalk.Walk (BRUSH_SIZE)) 
+				mStage++;
+		}
+		#endregion
+
+		#region Public methods
+		public Brush() : base() {
+			mOrigin = new Coord(0, 0);
+			mGridPosition = new Coord();
+			mWalk = new Coord();
+			mCurrentDistance = 0;
+			Valid = false;
+			mBBox.Clear();
+			mGridPosition.Clear();
+			mWalk.Clear();
+			mMesh.Clear();
+			mStage = BrushStage.Begin;
+  
+			if (!mPrepDone)
+				DoPrep ();
+		}
+
+		public void Set(int x, int y, int density) {
+			if (mOrigin.X == x * BRUSH_SIZE && mOrigin.Y == y * BRUSH_SIZE)
+				return;
+
+			mGridPosition.X = x;
+			mGridPosition.Y = y;
+
+			mCurrentDistance = (int) Math.Abs(density);
+			mOrigin.X = x * BRUSH_SIZE;
+			mOrigin.Y = y * BRUSH_SIZE;
+			mStage = BrushStage.Begin;
+			mMesh.Clear();
+			mBBox.Clear();
 		}
 
 		public void Update(long stop) {
 			while (SdlTick() < stop && !Ready ()) {
-				switch (stage) {
-				case BRUSH_STAGE_BEGIN:
-					if (!ZoneCheck ())
-						return;
-					stage++;
-				case BRUSH_STAGE_BUILD:
-					Build (stop);
-					break;
-				case BRUSH_STAGE_COMPILE:
-					if (mesh.Vertices ())
-						vbo.Create (&mesh);
-					else
-						vbo.Clear ();
-					stage++;
-					_valid = true;
-					break;
+				switch (mStage) {
+					case BrushStage.Begin:
+						if (!ZoneCheck())
+							return;
+						mStage++;
+					case BrushStage.Build:
+						Build(stop);
+						break;
+					case BrushStage.Compile:
+						if (mMesh.VertexCount != 0)
+							mVBO.Create(mMesh);
+						else
+							mVBO.Clear();
+						mStage++;
+						Valid = true;
+						break;
 				}
 			}
 		}
 
 		public void Render() {
 			//We need at least one successful build before we can draw.
-			if (!_valid)
+			if (!Valid)
 				return;
-			glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-			glTexParameteri (GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);	
-			glTexParameteri (GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);	
-			glDisable (GL_CULL_FACE);
-			vbo.Render ();
+			GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int) TextureMinFilter.Nearest);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int) TextureMagFilter.Nearest);
+			GL.Disable(EnableCap.CullFace);
+			mVBO.Render();
 		}
+		#endregion
 	}
 }
