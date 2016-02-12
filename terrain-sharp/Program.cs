f7660a2da1047f4c85c3815de162d34b86f0e7c7
Ewgenij Belzmann <ewgbel@gmail.com>
2016-02-12 16:52:09 +0100

    Renamed cpp files to cs

--- terrain-sharp/Program.cs
+++ terrain-sharp/Program.cs
@@ -13,7 +13,7 @@ namespace terrain_sharp {
 		static void Main() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			//Application.Run(new Form1());
 		}
 	}
 }
