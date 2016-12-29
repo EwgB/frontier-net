70b4fb6e094ffd33f6aad96eb95e5f22d988597c
Ewgenij Belzmann <ewgbel@gmail.com>
2016-12-29 01:47:05 +0100

    Added main C#+OpenTK project with some sample code

---
+++ FrontierSharp/Frontier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace FrontierSharp {
+	internal class Frontier : GameWindow {
+
+		protected override void OnLoad(EventArgs e) {
+			base.OnLoad(e);
+
+			Title = "Frontier";
+			GL.ClearColor(Color.CornflowerBlue);
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs e) {
+			base.OnRenderFrame(e);
+
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			var modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.LoadMatrix(ref modelview);
+
+			GL.Begin(PrimitiveType.Triangles);
+			{
+				GL.Color3(1.0f, 0.0f, 0.0f);
+				GL.Vertex3(-1.0f, -1.0f, 4.0f);
+
+				GL.Color3(0.0f, 1.0f, 0.0f);
+				GL.Vertex3(1.0f, -1.0f, 4.0f);
+
+				GL.Color3(0.0f, 0.0f, 1.0f);
+				GL.Vertex3(0.0f, 1.0f, 4.0f);
+			}
+			GL.End();
+
+			SwapBuffers();
+		}
+
+		protected override void OnResize(EventArgs e) {
+			base.OnResize(e);
+
+			GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+			var projection = Matrix4.CreatePerspectiveFieldOfView((float) Math.PI / 4, Width / (float) Height, 1.0f, 64.0f);
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.LoadMatrix(ref projection);
+		}
+
+	}
+}
---
+++ FrontierSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 {
+	internal class Program {
+		private static void Main(string[] args) {
+			using (var frontier = new Frontier()) {
+
+				frontier.Run(30.0);
+
+			}
+		}
+	}
+}
