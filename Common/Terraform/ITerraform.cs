45373e549592b5dc83ebb8bcd7736661f3125066
Ewgenij Belzmann <ewgbel@gmail.com>
2018-04-06 00:52:13 +0200

    Adds skeleton for Terraform module

---
+++ Common/Terraform/ITerraform.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontierSharp.Common.Terraform {
+    /// <summary>
+    /// A set of worker functions for IWorld.  Really, everything
+    /// here could go in IWorld/WorldImpl, except that it would be just too
+    /// damn big and unmanageable.
+    /// 
+    /// Still, this system isn't connected to anything else and it's only used
+    /// when IWorld is generating region data.
+    /// </summary>
+    public interface ITerraform : IModule {
+    }
+}
